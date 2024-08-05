74a2c501a030ceaf406e99b70fbfe6c2f1002bbe
joaquin muñoz <joaquinmatias99@gmail.com>
2024-08-05 16:36:10 -0300

    mejoras front

--- frontend/gestion-finanzas-app/ui/home/Home.jsx
+++ frontend/gestion-finanzas-app/ui/home/Home.jsx
@@ -1,7 +1,17 @@
+import './Home.css';
 
-export const Home = () => {
+const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-container">
+      <h1>Bienvenido a la APP para la Gestión de Finanzas</h1>
+      <p>
+        Esta página está diseñada para ayudarte a controlar y conocer la
+        gestión de gastos. Aquí podrás visualizar
+        tus gastos categorizados, agregar nuevos gastos, eliminar los existentes
+        y editar cualquier detalle relacionado con tus finanzas personales.
+      </p>
+    </div>
+  );
+};
+
 export default Home;
--- frontend/gestion-finanzas-app/ui/navBar/NavBar.jsx
+++ frontend/gestion-finanzas-app/ui/navBar/NavBar.jsx
@@ -1,11 +1,42 @@
-import "./NavBar.css";
+import  { useState, useEffect, useRef } from 'react';
+import './NavBar.css';
 
 export const NavBar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => setMenuOpen(!isMenuOpen);
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    //Listener para clics fuera del menú
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Limpia el listener cuando el componente se desmonte
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <nav className="menu">
       <a href="#" className="nav-link">Home</a>
       <a href="#" className="nav-link">Gestión</a>
       <a href="#" className="nav-link">Gráfico</a>
+      <div className="nav-link user-menu" onClick={toggleMenu} ref={menuRef}>
+        Usuario
+        {isMenuOpen && (
+          <div className="dropdown-menu">
+            <a href="#" className="dropdown-item">Perfil 1</a>
+            <a href="#" className="dropdown-item">Perfil 2</a>
+            <a href="#" className="dropdown-item">Crear nuevo perfil</a>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
